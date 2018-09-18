27af2f4a6700aaecd4cba98ffdef8085c75eef68
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2018-09-18 22:57:53 +0300

    Marti 18 septembrie

--- NN.h
+++ NN.h
@@ -8,13 +8,23 @@
 
 */
 
+#include <vector>
+typedef int Neuron; 
+typedef std::vector<Neuron> layer;
 
 class Net{
     private:
+        
 
     public:
-        void feedforward()
+        Net(const std::vector<unsigned int> &topology){
 
+        }
+        void feedforward( const std::vector<double>& inputVals); // beacause it doesn't make any change in the 
+                                                                 // inputVals vector
+        void backpropagation( const std::vector<double>& targetVals); 
+        void getResults( std::vector<double> results ); // we make changes to the results vector ( we write it )
+                                                        // there is no need for the const keyword
 
 
 };
--- mainApp.cpp
+++ mainApp.cpp
@@ -2,23 +2,31 @@
 // Neural Network ( feedforwoard model ) weather-predictor console application
 
 /*
-
-    -we have to feed our net with a topology 
-    
+ 
+    - topology : specify the architecture of the neutral net 
+      *for exemple, a topology like {3,2,1} =  specifies a 3 layes = size(toplogy)
+        neural net and then, topology[i], i = 1,size(toplogy) neurons / specific layer    
 
 */
+
 
 #include <iostream>
 #include "NN.h"
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 
 int main(){
-   
+    
+    vector<double> inputVals;
+    vector<double> targetVals;
+    vector<double> results;
+    vector<unsigned int> topology; // here we specifiy the topology of the neural net 
 
-
+    Net myNet(topology);
+    
     return 0;
 }
 
