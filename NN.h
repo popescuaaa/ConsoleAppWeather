55b9829ce4606f392e446442a2b7c0ed29a37299
Popescu Andrei <andreigabrielpopescu556@gmail.com>
2018-09-20 02:14:53 +0300

    Deta Rule and documentation on basics

--- NN.h
+++ NN.h
@@ -9,22 +9,71 @@
 */
 
 #include <vector>
-typedef int Neuron; 
+#include <iostream>
+
+struct Connection{
+    double weight;
+    double deltaWeight;
+
+};
+
+class Neuron;
+
 typedef std::vector<Neuron> layer;
 
+// ********************** Class Neuron ********************
+// we define the class here as we'll use the layer data type 
+
+class Neuron{
+    public:
+        Neuron();
+
+    private:
+        // the most important piece of data in a Neuron
+        double m_outputValue;
+        // a Neuron also contains a specific weight which is used to feed 
+        // the other Neurons, in our case will be specified and fiexd
+        // Note: there are specific type of neural nets with specific weights 
+        // calculated even during the process of learning in order to minimize the
+        // erros.
+        std::vector<Connection> m_outputWeights;
+        // there is one weight for every Neuron which is connected
+
+
+};
+
+// ********************** Class Net ***********************
 class Net{
     private:
-        
+        std::vector<layer> m_layers;
 
     public:
-        Net(const std::vector<unsigned int> &topology){
-
-        }
-        void feedforward( const std::vector<double>& inputVals); // beacause it doesn't make any change in the 
+        Net(const std::vector<unsigned int> &topology);
+        void feedforward( const std::vector<double>& inputVals){}; // beacause it doesn't make any change in the 
                                                                  // inputVals vector
-        void backpropagation( const std::vector<double>& targetVals); 
-        void getResults( std::vector<double> results ); // we make changes to the results vector ( we write it )
+        void backpropagation( const std::vector<double>& targetVals){}; 
+        void getResults( std::vector<double> results ){}; // we make changes to the results vector ( we write it )
                                                         // there is no need for the const keyword
 
 
 };
+
+ Net::Net(const std::vector<unsigned int> &topology){
+     int numLayers = topology.size();
+     
+     
+     // for every layer in numLayers range we'll add a new layer to  m_layers 
+     for( unsigned int i =0;i < numLayers;i++ ){
+         m_layers.push_back(layer());
+
+         // We have added a new layer, now we'll fill it with Neurons
+         // and we'll add a bias neuron to the layer
+         for(unsigned int numNeurons = 0;numNeurons <= topology[i];numNeurons++ ){
+             // we go further with one position because we'll have an aditional BIAS neuron 
+             // with a fixed value
+             m_layers.back().push_back( Neuron() );
+        
+
+         } 
+     }
+ }
--- mainApp.cpp
+++ mainApp.cpp
@@ -25,6 +25,10 @@ int main(){
     vector<double> results;
     vector<unsigned int> topology; // here we specifiy the topology of the neural net 
 
+    topology.push_back(3);
+    topology.push_back(2);
+    topology.push_back(1);
+    
     Net myNet(topology);
     
     return 0;
